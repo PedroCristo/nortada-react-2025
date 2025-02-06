29a4b6dc2c05061f8763276a9c8a65e668273563
Pedro Cristo <pcristo35@gmail.com>
2025-02-06 13:24:17 +0000

    Change path name for Footer_pt

--- src/pages/pt/MenuPt.jsx
+++ src/pages/pt/MenuPt.jsx
@@ -2,7 +2,7 @@ import TopBarPt from "../../components/pt/TopBarPt";
 import HeaderPt from "../../components/pt/HeaderPt";
 import HeaderMenu from "../../components/UI/HeaderMenu";
 import MenuSectionPt from "../../components/pt/MenuSectionPt";
-import FooterPt from "../../components/pt/fOOTERpT.JSX";
+import Footer_Pt from "../../components/pt/Footer_Pt";
 import CookiesAlertPt from "../../components/pt/CookiesAlertPt";
 
 function MenuPt() {
@@ -12,7 +12,7 @@ function MenuPt() {
       <HeaderPt />
       <HeaderMenu menu_title={"Ementa"} />
       <MenuSectionPt />
-      <FooterPt />
+      <Footer_Pt />
       <CookiesAlertPt />
     </div>
   );
--- src/pages/pt/PrivacyCookiesPolicyPt.jsx
+++ src/pages/pt/PrivacyCookiesPolicyPt.jsx
@@ -1,7 +1,7 @@
 import TopBarPt from "../../components/pt/TopBarPt";
 import HeaderPt from "../../components/pt/HeaderPt";
 import HeaderMenu from "../../components/UI/HeaderMenu";
-import FooterPt from "../../components/pt/fOOTERpT.JSX";
+import Footer_Pt from "../../components/pt/Footer_Pt";
 import CookiesAlertPt from "../../components/pt/CookiesAlertPt";
 import PrivacyCokiesPolicyPt from "../../components/pt/PrivacyAndCokiesPolicyPt";
 
@@ -12,7 +12,7 @@ function PrivacyCokkiesPolicyPt() {
       <HeaderPt />
       <HeaderMenu menu_title={"Política de Privacidade e Cookies"}/>
       <PrivacyCokiesPolicyPt />
-      <FooterPt />
+      <Footer_Pt />
       <CookiesAlertPt />
     </div>
   );
