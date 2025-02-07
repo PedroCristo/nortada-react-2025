08668a4a8dd453a8aca2e29ba420a6a6187cff9c
Pedro Cristo <pcristo35@gmail.com>
2025-02-07 15:23:52 +0000

    Remove preloader for testing

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <ScrollToTop />
       <App />
       <BackTop />
-      <Preloader />
+      {/* <Preloader /> */}
     </Router>
   </React.StrictMode>
 );
