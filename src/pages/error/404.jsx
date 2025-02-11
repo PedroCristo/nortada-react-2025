703051925bc21af1232c99a199f3fce766ff4619
Pedro Cristo <pcristo35@gmail.com>
2025-02-11 15:26:59 +0000

    Fix url for images and add btn class for btns in the 404 page

--- src/pages/error/404.jsx
+++ src/pages/error/404.jsx
@@ -3,27 +3,33 @@ import { Link } from "react-router-dom";
 const PageNotFound = () => {
   return (
     <>
-    <div className="page404 set-bg">
-      <div className="content-box">
-        <h1 className="title">404</h1>
-        <div className="content-item">
-          <img src="../public/images/extras/flags/pt-flag.jpg" alt="Flag Image" />
-          <h2>Página Não Encontrada</h2>
+      <div className="page404 set-bg">
+        <div className="content-box">
+          <h1 className="title">404</h1>
+          <div className="content-item">
+            <img
+              src="../public/images/extras/flags/pt-flag.jpg"
+              alt="Flag Image"
+            />
+            <h2>Página Não Encontrada</h2>
+          </div>
+          <div className="content-item">
+            <img
+              src="../public/images/extras/flags/gb-flag.png"
+              alt="Flag Image"
+            />
+            <h2>Page Not Foud</h2>
+          </div>
         </div>
-        <div className="content-item">
-          <img src="../public/images/extras/flags/gb-flag.png" alt="Flag Image" />
-          <h2>Page Not Foud</h2>
+        <div className="btn-box">
+          <Link to="/" className="btn">
+            <i class="bi bi-arrow-left"></i> Voltar
+          </Link>
+          <Link to="/home-en" className="btn">
+          <i class="bi bi-arrow-left"></i> Go Back
+          </Link>
         </div>
       </div>
-      <div className="btn-box">
-        <Link to="/" className="book-a-table-btn">
-          Voltar
-        </Link>
-        <Link to="/home-en" className="book-a-table-btn">
-          Go Back
-        </Link>
-      </div>
-    </div>
     </>
   );
 };
