23d2e8aac0bf59834863e55fd578f2e231653ec2
Pedro Cristo <pcristo35@gmail.com>
2026-06-25 09:05:33 +0100

    Add new images to the gallery

--- src/data/global/gallery-images.js
+++ src/data/global/gallery-images.js
@@ -3,44 +3,90 @@
 const galleryDataImages = [
     {
         id: 1,
-        image_url_desktop: "/images/desktop/gallery/nortada_3.jpg",
-        image_url_tablet: "",
+        image_url_desktop: "/images/mobile/gallery/nortada_esplananada_9-16_1.jpg",
+        image_url_tablet: "/images/mobile/gallery/nortada_esplananada_9-16_1.jpg",
     },
     {
-        id: 1,
-        image_url_desktop: "/images/desktop/gallery/nortada_4.jpg",
-        image_url_tablet: "",
+        id: 2,
+        image_url_desktop: "/images/mobile/gallery/nortada_sala_9-16_3.jpg",
+        image_url_tablet: "/images/mobile/gallery/nortada_sala_9-16_3.jpg",
     },
     {
-        id: 1,
-        image_url_desktop: "/images/desktop/gallery/nortada_5.jpg",
-        image_url_tablet: "",
+        id: 3,
+        image_url_desktop: "/images/mobile/gallery/nortada_balcao_9-16_2.jpg",
+        image_url_tablet: "/images/mobile/gallery/nortada_balcao_9-16_2.jpg",
     },
     {
-        id: 1,
-        image_url_desktop: "/images/desktop/gallery/nortada_10.jpg",
-        image_url_tablet: "",
+        id: 4,
+        image_url_desktop: "/images/mobile/gallery/nortada_sala_9-16_5.jpg",
+        image_url_tablet: "/images/mobile/gallery/nortada_sala_9-16_5.jpg",
     },
     {
-        id: 1,
-        image_url_desktop: "/images/desktop/gallery/nortada_11.jpg",
-        image_url_tablet: "",
+        id: 5,
+        image_url_desktop: "/images/mobile/gallery/nortada_esplananada_9-16_6.jpg",
+        image_url_tablet: "/images/mobile/gallery/nortada_esplananada_9-16_6.jpg",
     },
     {
-        id: 1,
-        image_url_desktop: "/images/desktop/gallery/nortada_2.jpg",
-        image_url_tablet: "",
+        id: 6,
+        image_url_desktop: "/images/mobile/gallery/nortada_esplananada_9-16_1.jpg",
+        image_url_tablet: "/images/mobile/gallery/nortada_esplananada_9-16_1.jpg",
     },
-    {
-        id: 1,
-        image_url_desktop: "/images/desktop/gallery/nortada_13.jpg",
-        image_url_tablet: "",
-    },
-    {
-        id: 1,
-        image_url_desktop: "/images/desktop/gallery/nortada_9.jpg",
-        image_url_tablet: "",
-    },
+    // {
+    //     id: 7,
+    //     image_url_desktop: "/images/mobile/gallery/nortada_esplananada_9-16_6.jpg",
+    //     image_url_tablet: "/images/mobile/gallery/nortada_esplananada_9-16_6.jpg",
+    // },
+    // {
+    //     id: 8,
+    //     image_url_desktop: "/images/mobile/gallery/nortada_esplananada_9-16_1.jpg",
+    //     image_url_tablet: "/images/mobile/gallery/nortada_esplananada_9-16_1.jpg",
+    // },
 ]
 
 export default galleryDataImages;
+
+
+// const galleryDataImages = [
+//     {
+//         id: 1,
+//         image_url_desktop: "/images/desktop/gallery/nortada_3.jpg",
+//         image_url_tablet: "",
+//     },
+//     {
+//         id: 1,
+//         image_url_desktop: "/images/desktop/gallery/nortada_4.jpg",
+//         image_url_tablet: "",
+//     },
+//     {
+//         id: 1,
+//         image_url_desktop: "/images/desktop/gallery/nortada_5.jpg",
+//         image_url_tablet: "",
+//     },
+//     {
+//         id: 1,
+//         image_url_desktop: "/images/desktop/gallery/nortada_10.jpg",
+//         image_url_tablet: "",
+//     },
+//     {
+//         id: 1,
+//         image_url_desktop: "/images/desktop/gallery/nortada_11.jpg",
+//         image_url_tablet: "",
+//     },
+//     {
+//         id: 1,
+//         image_url_desktop: "/images/desktop/gallery/nortada_2.jpg",
+//         image_url_tablet: "",
+//     },
+//     {
+//         id: 1,
+//         image_url_desktop: "/images/desktop/gallery/nortada_13.jpg",
+//         image_url_tablet: "",
+//     },
+//     {
+//         id: 1,
+//         image_url_desktop: "/images/desktop/gallery/nortada_9.jpg",
+//         image_url_tablet: "",
+//     },
+// ]
+
+// export default galleryDataImages;
